fcc5a0d8467eb8840ed1beff7d8a4d9ece1a9469
unknown <femiabimbola6@gmail.com>
2020-11-17 15:12:08 +0100

    project-setup-2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import Debug from "debug";
 
-const App = express();
+const app = express();
 const port = process.env.PORT || 8000 ;
 
 const devDebug = Debug('dev')
 
-App.listen(port, () => {
+app.get('/', (req, res) => {
+    res.status(200).send(`The root application is live`)
+})
+
+
+app.listen(port, () => {
     devDebug(`The server is running on port https://localhost:${port}`)
 })

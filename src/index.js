b45517788f344b3d53843289d2a6768bdeecd21f
unknown <femiabimbola6@gmail.com>
2020-11-18 15:22:41 +0100

    creating routes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ app.get('/', (req, res) => {
     res.status(200).send(`The root application is live`)
 })
 
+app.use('/routes', (req, use) => {
+    res.status(200).send("routes index")
+})
+
 
 app.listen(port, () => {
-    devDebug(`The server is running on port https://localhost:${port}`)
+    devDebug(`The server is running on port http://localhost:${port}`)
 })

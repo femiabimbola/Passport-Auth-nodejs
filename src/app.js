4c8b1c834506f3986a7eca60b437f9b11c6455b7
unknown <femiabimbola6@gmail.com>
2020-11-19 09:12:52 +0100

    fixed the indexjs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 import express from 'express';
-// import Debug from "debug";
 // import routes from './routes'
 
 const app = express();
-// const port = process.env.PORT || 8000 ;
-
-// const devDebug = Debug('dev')
 
 app.get('/', (req, res) => {
     res.status(200).send(`The root application is live`)
@@ -14,6 +10,4 @@ app.get('/', (req, res) => {
 // app.use('/routes', routes);
 
 
-// app.listen(port, () => {
-//     devDebug(`The server is running on port http://localhost:${port}`)
-// })
+export default app;

cc19fa5157d5bd2a4674621079d9c790ac5247e4
unknown <femiabimbola6@gmail.com>
2020-11-24 15:29:51 +0100

    mongose connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import routes from './routes'
-import expressLayout from 'express-ejs-layouts'
+import expressLayout from 'express-ejs-layouts';
+import mongoose from 'mongoose';
+import { db } from '../config/keys';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).send(`The root application is live`)
 });
+
+ mongoose.connect(db, { useNewUrlParser: true});
 
 app.use(expressLayout); // this has to be about the view engine
 app.set('view engine', 'ejs');

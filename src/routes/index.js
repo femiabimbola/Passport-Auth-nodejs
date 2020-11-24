3b1fff9edb553ed51f6e0ac9dc37f11d7d64fe0c
unknown <femiabimbola6@gmail.com>
2020-11-24 13:26:24 +0100

    bug fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import usersRoutes from './users';
 const defaultRouter = express.Router();
 
 defaultRouter.get('/', (req, res) => {
-    // res.status(200).send('Welcome to the default route')
     res.status(200).render('welcome')
 });
 

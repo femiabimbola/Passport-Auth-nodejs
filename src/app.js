76cf52706373db8e66e402a7533f61ec939723e0
unknown <femiabimbola6@gmail.com>
2020-11-27 12:59:07 +0100

    model setup

--- src/app.js
+++ src/app.js
@@ -7,9 +7,19 @@ import Debug from 'debug';
 
 const app = express();
 
+app.use(expressLayout); // this has to be about the view engine
+app.set('view engine', 'ejs');
+
+//Bodypaser
+//Extended is false because req.body will give info
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     res.status(200).send(`The root application is live`)
 });
+app.use('/users', routes);
 
 const devDebug = Debug('dev');
 
@@ -17,11 +27,6 @@ const devDebug = Debug('dev');
  .then(() => devDebug('Mongodb connected....'))
  .catch(err => devDebug(err));
 
-app.use(expressLayout); // this has to be about the view engine
-app.set('view engine', 'ejs');
-
-
-app.use('/users', routes);
 
 app.all('*', (req, res, next) => {
     res.status(404).send('Nothing here, check the documentation')
